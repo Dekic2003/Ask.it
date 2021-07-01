4f9080b4af22b307ea9e894b9805a3f013c87b0c
Dekic2003 <Dekic2003@users.noreply.github.com>
2021-07-01 17:31:26 +0200

    :lipstick:Added question card

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -1,6 +1,7 @@
 import React,{useEffect} from 'react'
 import {useDispatch,useSelector} from "react-redux";
 import fetchQuestions from "../store/actions/questions";
+import QuestionCard from "../Components/questionCard";
 
 const Homepage = () => {
 
@@ -14,9 +15,32 @@ const Homepage = () => {
 
 
     return(
-        <div>
-            <p>Homepage</p>
-        </div>
+        <main className="container">
+            <div className="row ">
+                <div className="col-md-8">
+                    {
+                        Questions.map((item,i)=>{
+                            return(
+                                    <QuestionCard data={item}/>
+                            )
+                        })
+                    }
+                </div>
+                <div className="col-md-4">
+                    <div className="position-sticky" style={{top:'2rem'}}>
+                        <div className="p-2">
+                            <h4 className="fst-italic">Elsewhere</h4>
+                            <ol className="list-unstyled">
+                                <li><a href="#">GitHub</a></li>
+                                <li><a href="#">Twitter</a></li>
+                                <li><a href="#">Facebook</a></li>
+                            </ol>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </main>
     )
 }
 export default Homepage
