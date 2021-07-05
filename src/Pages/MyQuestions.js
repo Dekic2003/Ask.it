68e2317683b96499da3ca25de2894fd0a0f3fd5b
Dekic2003 <Dekic2003@users.noreply.github.com>
2021-07-05 23:19:05 +0200

    :bug:Fetch My questions fix

--- src/Pages/MyQuestions.js
+++ src/Pages/MyQuestions.js
@@ -1,8 +1,8 @@
 import React, {useEffect, useState} from 'react'
 import {useDispatch,useSelector} from "react-redux";
-import fetchQuestions from "../store/actions/questions";
 import QuestionCard from "../Components/questionCard";
 import Header from "../Components/header";
+import fetchMyQuestions from "../store/actions/myQuestions";
 
 const MyQuestions = () => {
 
@@ -13,7 +13,7 @@ const MyQuestions = () => {
 
 
     useEffect(()=>{
-        dispatch(fetchQuestions(User.id));
+        dispatch(fetchMyQuestions(User.id));
     },[])
 
 
